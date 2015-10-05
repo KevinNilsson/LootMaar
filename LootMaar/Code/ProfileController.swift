015dedf33feb598e2838d6b26374fa5d7c0b306d
kang <kang.strong1206@gmail.com>
2015-10-06 01:04:50 +0200

    Profile update

--- LootMaar/Code/ProfileController.swift
+++ LootMaar/Code/ProfileController.swift
@@ -10,8 +10,31 @@ import UIKit
 
 class ProfileController: UIViewController {
 
+    
+    
+    @IBOutlet weak var viewLogo: UIView!
+    
+    @IBOutlet weak var viewPoints: UIView!
+    
+    @IBOutlet weak var viewFacebook: UIView!
+    
     @IBOutlet weak var scrollCtrl: UIScrollView!
+    @IBOutlet weak var viewEmail: UIView!
+    
+    
+    @IBOutlet weak var viewCountry: UIView!
+    
+    @IBOutlet weak var viewState: UIView!
+    
+    @IBOutlet weak var viewNews2: UIView!
+    
+    @IBOutlet weak var viewNews1: UIView!
+    
+    @IBOutlet weak var viewNews3: UIView!
     //@IBOutlet weak var mainScroller: UIScrollView!
+    @IBOutlet weak var viewSocialMedia: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,7 +49,9 @@ class ProfileController: UIViewController {
 
     
     override func viewDidAppear(animated: Bool) {
-        scrollCtrl.contentSize = CGSizeMake(scrollCtrl.frame.size.width,1400);
+        
+        let height = viewLogo.frame.height + viewPoints.frame.height + viewFacebook.frame.height + viewEmail.frame.height + viewCountry.frame.height + viewState.frame.height + viewNews2.frame.height + viewNews1.frame.height + viewNews3.frame.height + viewSocialMedia.frame.height + 100
+        scrollCtrl.contentSize = CGSizeMake(scrollCtrl.frame.size.width,height);
 
     }
     
