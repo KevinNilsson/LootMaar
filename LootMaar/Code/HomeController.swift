ddf1927b34c22a08ee9ec66edf417e5c4e7b2697
kang <kang.strong1206@gmail.com>
2015-10-06 05:01:10 +0200

    XXXXXXXX

--- LootMaar/Code/HomeController.swift
+++ LootMaar/Code/HomeController.swift
@@ -10,7 +10,25 @@ import UIKit
 
 class HomeController : UIViewController
 {
+
+    // Top menu constant space
+    @IBOutlet weak var topMenuSpace: NSLayoutConstraint!
+    
+    
+    // Showing or hidding top menu when clicking the trigger button
+    @IBAction func onTriggerMenu(sender: AnyObject) {
         
+        if topMenuSpace.constant == 0.0 {
+            topMenuSpace.constant = 50
+        } else {
+            topMenuSpace.constant = 0
+        }
+    }
+    
+    
+    
+    
+    
     
     
 }
